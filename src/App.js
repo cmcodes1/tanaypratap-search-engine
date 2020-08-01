a2e8ed07a0d33a84851ed2a8f6c2cfe222edd98e
C M Pandey <34341671+Cm-Champ@users.noreply.github.com>
2020-08-01 15:17:49 +0530

    code refactored

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import './App.css';
-import './style.css';
-import { Input, Footer, Card, CardBody, CardTitle } from 'mdbreact';
-import contentList from './Content.json';
+import React from "react";
+import "./App.css";
+import "./style.css";
+import { Input, Footer, Card, CardBody, CardTitle } from "mdbreact";
+import contentList from "./Content.json";
 
 class App extends React.Component {
-
   state = {
-    search: ""
-  }
+    search: "",
+  };
 
-  renderContent = content => {
-    
+  renderContent = (content) => {
     return (
       <div className="col-md-3" style={{ marginTop: "20px" }}>
         <Card>
           <CardBody>
             <CardTitle title={content.question}>
-              <a href={content.link} target="_blank" rel="noopener noreferrer">{content.question}</a>
+              <a href={content.link} target="_blank" rel="noopener noreferrer">
+                {content.question}
+              </a>
             </CardTitle>
           </CardBody>
         </Card>
@@ -25,31 +25,31 @@ class App extends React.Component {
     );
   };
 
-  onChange = e => {
+  onChange = (e) => {
     this.setState({ search: e.target.value });
   };
 
   render() {
-    
     const { search } = this.state;
-    const filteredContent = contentList.filter(content => {
-      return content.question.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    const filteredContent = contentList.filter((content) => {
+      return (
+        content.question.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      );
     });
 
     return (
       <div className="flyout">
-
         <main style={{ marginTop: "4rem" }}>
           <div className="container">
-            
             <div>
-
-              <div style={{color: "#0078d7"}}>
+              <div style={{ color: "#0078d7" }}>
                 <center>
-                <h1>the tanay pratap search engine</h1><br/><br/>
+                  <h1>the tanay pratap search engine</h1>
+                  <br />
+                  <br />
                 </center>
               </div>
-              
+
               <div>
                 <center>
                   <Input
@@ -62,27 +62,45 @@ class App extends React.Component {
 
               <div>
                 <center>
-                  <h5>search for anything tanay has ever said<br/></h5><br/><br/>
+                  <h5>
+                    search for anything tanay has ever said
+                    <br />
+                  </h5>
+                  <br />
+                  <br />
                 </center>
               </div>
-
             </div>
 
             <div className="row">
-              {filteredContent.map(content => {
+              {filteredContent.map((content) => {
                 return this.renderContent(content);
               })}
             </div>
-
           </div>
         </main>
 
         <Footer color="blue">
           <p className="footer-copyright mb-0" style={{ marginTop: 50 }}>
-            &copy; {new Date().getFullYear()} | Made by <a href="https://cmcodes1.github.io/" target="_blank" rel="noopener noreferrer">cmcodes</a> for <a href="https://tanaypratap.com/" target="_blank" rel="noopener noreferrer">tanaypratap</a> with ❤️ & React.
+            &copy; {new Date().getFullYear()} | Made by{" "}
+            <a
+              href="https://cmcodes1.github.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              cmcodes
+            </a>{" "}
+            for{" "}
+            <a
+              href="https://tanaypratap.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              tanaypratap
+            </a>{" "}
+            with ❤️ & React.
           </p>
         </Footer>
-
       </div>
     );
   }
